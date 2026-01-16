e6cdbeb501da5b48a1b0e07cc21eb69bb516ca43
F-A-M-Ji <jirashuda20@gmail>
2026-01-16 09:38:54 +0700

    16-01-2026

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -10,13 +10,13 @@ class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        User::create([
-            'username' => 'admin01',      // 7 หลักพอดี
-            'password' => '12345678',     // ใส่รหัสปกติ Model จะ Hash ให้เองเพราะเราตั้ง cast ไว้
-            'firstname' => 'Admin',
-            'lastname' => 'System',
-            'province_code' => '10',      // สมมติ กทม.
-            'role' => 'ADMIN',
-        ]);
+        // User::create([
+        //     'username' => 'admin01',      // 7 หลักพอดี
+        //     'password' => '12345678',     // ใส่รหัสปกติ Model จะ Hash ให้เองเพราะเราตั้ง cast ไว้
+        //     'firstname' => 'Admin',
+        //     'lastname' => 'System',
+        //     'province_code' => '10',      // สมมติ กทม.
+        //     'role' => 'ADMIN',
+        // ]);
     }
 }
